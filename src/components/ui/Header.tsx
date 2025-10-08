c378d6bbc723c14f52ed53b201c2cc6b5e4dca94
RTSaitama <rtsaitama@outlook.com>
2025-10-08 03:27:11 +0300

    filters changed

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,16 +5,16 @@ export const Header = () => {
 <header className=" ">
       <h1>Rick & Morty Wiki</h1>
       <nav className="navigation flex flex-row gap-4">
-        <Link href="/characters" >
-          <p   className="rick-morty-card-link">Characters</p>
+        <Link href="/character" >
+          <p   className="rick-morty-card-link">Character </p>
 
         </Link>
-        <Link href="/episodes" >
-          <p   className="rick-morty-card-link">Episodes</p>
+        <Link href="/episode" >
+          <p   className="rick-morty-card-link">Episode </p>
 
         </Link> 
-        <Link href="/locations" >
-          <p   className="rick-morty-card-link">Locations</p>
+        <Link href="/location" >
+          <p   className="rick-morty-card-link">Location </p>
         </Link>
       </nav>
     </header>
