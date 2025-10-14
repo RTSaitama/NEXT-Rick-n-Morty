b4d369877adf880909ff3fe76f9dd45ad88db168
RTSaitama <rtsaitama@outlook.com>
2025-10-14 16:04:48 +0300

    searchParams removed from base page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
-export default async function HomePage({ searchParams }: { searchParams: { page?: string } }) {
+export default async function HomePage() {
 redirect('/character')
 }
