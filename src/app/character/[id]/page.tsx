2dd8a96274b21c2a93b4d00e73841ab1aa4f3928
RTSaitama <rtsaitama@outlook.com>
2025-10-14 16:11:43 +0300

    fixed async Character Page params as it should be with async

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -2,8 +2,9 @@ import { fetchCharacter } from '@/app/lib/api';
 import CharacterCard from '@/components/ui/CharacterCard';
 import type { CharacterDetails } from '@/typedefs/typedefs';
 
-export default async function CharacterPage({ params }: { params: { id: string } }) {
-  const character: CharacterDetails = await fetchCharacter(Number(params.id));
+export default async function CharacterPage({  params }: {   params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const character: CharacterDetails = await fetchCharacter(Number(id));
 
   return (
     <CharacterCard character={character}/>
